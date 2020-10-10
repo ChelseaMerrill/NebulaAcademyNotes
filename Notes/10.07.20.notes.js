88f710626854af1a0da45e487488568d617da8a6
Chelsea Merrill <chelmer1223@aol.com>
2020-10-10 13:58:28 -0400

    October_10_2020

---
+++ Notes/10.07.20.notes.js
@@ -0,0 +1,13 @@
+var x =9; 
+console.log(x);
+
+var chelsea = "hello";
+console.log(chelsea);
+
+let k = "practice";
+console.log(k);
+
+// when declaring values you can use single quote, double quote or backticks
+//but when using temperal litterals you can only use backticks
+// template literal -LOOK THIS UP
+//console.log(`this new string is ${newString} it is h-llo!! this is hello now ${newVariable}`
