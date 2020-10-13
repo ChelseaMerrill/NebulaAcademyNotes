983a8fe7a3112a4c00a6cc923c88d82cb63f93ec
Chelsea Merrill <chelmer1223@aol.com>
2020-10-12 22:51:28 -0400

    10.10.Assignment

---
+++ Assignments/10.10.assignment.js
@@ -0,0 +1,39 @@
+//level 0
+//Q0: Declare a string with the value equal to your name
+let Q0 = `CHELSEA`
+//Q1: Given the string, 'WE ARE SHOUTING!' log 'we are shouting'
+let Q1 = `WeAreShouting`
+console.log(Q1) 
+
+//level 1
+//Q2: Declare a string and log its length
+let Q2 = `question 2`
+console.log(Q2.length)
+//10
+//Q3: Given the string 'Hello World!', log the 6th character.
+let Q3 = `Hello World!`
+console.log(Q3[6])
+//W
+
+//level 2
+//Q4: Given the string 'Welcome World?', replace the question mark with an exclamation
+let Q4 = `Welcome World?`
+console.log(Q3.replace("?" , "!"))
+//Hello World!
+
+//level 3
+//Q5: given any string, log the first and last characters
+let Q5 = "first and last characters"
+console.log(Q5.length)
+//25
+console.log(Q5.charAt(0))
+//f
+console.log(Q5[Q5.length - 1])
+//s
+
+
+//level 4
+//Q6: given the string 'welcome to my website', what is the character at the 7th index ? 
+let Q6 = `welcome to my website`
+console.log(Q6.charAt[7])
+//undefined
