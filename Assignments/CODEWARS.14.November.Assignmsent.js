0ca470a7820f4ac99610ac1e42469589e78b4cb1
Chelsea Merrill <chelmer1223@aol.com>
2020-11-13 20:55:20 -0500

    nov 13

---
+++ Assignments/CODEWARS.14.November.Assignmsent.js
@@ -0,0 +1,20 @@
+// Problem 1
+//https://www.codewars.com/kata/515e271a311df0350d00000f/solutions/javascript
+
+//Problem 2
+//https://www.codewars.com/kata/568dcc3c7f12767a62000038/solutions/javascript
+
+//Problem 3
+//https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/solutions/javascript
+
+//Problem 4
+//https://www.codewars.com/kata/55a70521798b14d4750000a4/solutions/javascript
+
+//Problem 5
+//https://www.codewars.com/kata/571d42206414b103dc0006a1/solutions/javascript
+
+//Problem 6
+//https://www.codewars.com/kata/57d814e4950d8489720008db/solutions/javascript
+
+//Problem 7
+//https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/solutions/javascript 
