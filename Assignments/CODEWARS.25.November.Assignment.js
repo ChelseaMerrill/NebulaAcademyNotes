3239a391704ad34fb518ac73b74f9c15b4d68d44
Chelsea Merrill <chelmer1223@aol.com>
2020-11-25 18:15:23 -0500

    hw

--- Assignments/CODEWARS.25.November.Assignment.js
+++ Assignments/CODEWARS.25.November.Assignment.js
@@ -60,3 +60,17 @@ function repeatStr (n, s) {
 // Problem #5
 // fakeBin
 // https://www.codewars.com/kata/57eae65a4321032ce000002d/train/javascript
+function fakeBin(x){
+    x = x.replace(/1/g, '0')
+    x = x.replace(/2/g, '0')
+    x = x.replace(/3/g, '0')
+    x = x.replace(/4/g, '0')
+    x = x.replace(/5/g, '1')
+    x = x.replace(/6/g, '1')
+    x = x.replace(/7/g, '1')
+    x = x.replace(/8/g, '1')
+    x = x.replace(/9/g, '1')
+      return x
+} 
+    console.log(fakeBin('45385593107843568'))
+ // '01011110001100111'
